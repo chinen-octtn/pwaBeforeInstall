5f23aa1fb2abfd2ae870002343231732070c0e5e
maashi <octtn8c@gmail.com>
2022-11-16 00:08:25 +0900

    add: mini infobar

--- public/js/script.js
+++ public/js/script.js
@@ -3,29 +3,4 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(() => {
     console.log("Service worker registered.");
   });
-
-  // インストールイベントを再利用するために変数に格納する
-  let eventPrompt = "";
-
-  // インストール前のイベント
-  window.addEventListener("beforeinstallprompt", (event) => {
-    // インストールイベントを格納
-    eventPrompt = event;
-
-    // デフォルトのmini-infobarは非表示にする
-    // eventPrompt.preventDefault();
-
-    // インストールしたかキャンセルしたかを確認する
-    eventPrompt.userChoice.then((choiceResult) => {
-      console.log(choiceResult.outcome); // "accepted" or "dismissed"
-    });
-  });
-
-  // インストールボタンを押したら、installメニューを表示する
-  const button = document.querySelector("#installButton");
-
-  button.addEventListener("click", () => {
-    console.log("click");
-    eventPrompt.prompt();
-  });
 }
